0270a5388921f5a99bae93deafe718f401d439dc
Julien Law <julienlaw1@gmail.com>
2013-10-23 16:16:25 -0700

    added the hash checking function

--- pass-q2.py
+++ pass-q2.py
@@ -34,3 +34,25 @@ def generateRandomString(input_set, max_chars):
     return ''.join(random.choice(input_set) for x in range(max_chars))
     
 #print generateRandomString(input_set, max_chars)
+
+def hashCheck(input_string):
+    """
+       This function takes an input string and compares it with the hash of a randomly generated string.
+       If the hash is found, the randomly generated string is returned
+    """
+    value = input_string.lower()
+    password = generateRandomString(input_set, max_chars)
+    while(True):
+        if generateSha1(salt + password) == value:
+            break
+        elif generateSha1(password + salt) == value:
+            break
+        password = generateRandomString(input_set, max_chars)
+    return password
+    
+print 'Hashed password: ' + password
+print 'Salt: ' + salt
+    
+print hashCheck(no_salt)
+        
+    
