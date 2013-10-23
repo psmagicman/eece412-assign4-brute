9e179e10767cf7e936808327b2ed2160a87a3acb
Julien Law <julienlaw1@gmail.com>
2013-10-23 15:49:48 -0700

    added a random generator

--- pass-q2.py
+++ pass-q2.py
@@ -5,10 +5,14 @@ Description: Brute force password cracker for question 2
 """
 
 import hashlib
+import random
+import string
 
 password = 'Aw30898863401E5C09AED7311AF30DB00FF451209A'
 salt = 'Aw'
 no_salt = '30898863401E5C09AED7311AF30DB00FF451209A'
+input_set = 'abcdefghijklmnopqarstuvqxyzABCDEFGHIJKLMNOPQRSTUVWXWZ1234567890!@#$%^&*()_+-='
+max_chars = 6
 
 #print no_salt.__len__()
 
@@ -22,3 +26,11 @@ def generateSha1(password):
     return m.hexdigest()
     
 #print generateSha1('lol')
+
+def generateRandomString(input_set, max_chars):
+    """
+    This function will generate and return a string of random chars
+    """
+    return ''.join(random.choice(input_set) for x in range(max_chars))
+    
+#print generateRandomString(input_set, max_chars)
